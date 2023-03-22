ad0d35dd31171e6d8430193c7c91bd9febde22a4
Josh Charpentier <charpentier.josh@gmail.com>
2023-03-21 23:15:35 -0500

    Finalized no locaiton found page

--- src/pages/NoLocation.tsx
+++ src/pages/NoLocation.tsx
@@ -1,8 +1,18 @@
+import { Button } from "react-bootstrap"
+import { useNavigate } from "react-router-dom";
+
 export default function NoMatch() {
+    const navigate = useNavigate()
+
+    function handleHome() {
+        navigate('/')
+    }
+
     return (
         <div className="text-center">
-            <h1>No location could be found</h1>
-            <h1>Try making sure your location is enabled in both your browser & your device settings</h1>
+            <h2>No location could be found!</h2>
+            <h2>Try making sure your location is enabled in both your browser & your device settings</h2>
+            <Button  onClick={handleHome} className="home--new--button shadow-none">Back to Home</Button>
         </div>
     )
 }
